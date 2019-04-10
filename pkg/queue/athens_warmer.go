09715eb5c6bf1b580d880e01173a4cec213f0389
Aaron Schlesinger <aaron@ecomaz.net>
2019-04-10 14:07:06 -0700

    implementing dumb crawler

--- pkg/queue/athens_warmer.go
+++ pkg/queue/athens_warmer.go
@@ -1,1 +1,14 @@
 package queue
+
+import "github.com/arschles/crathens/pkg/log"
+
+func athensWarmer(coord *coordinator) {
+	for range coord.ticker.C {
+		select {
+		case <-coord.ctx.Done():
+			return
+		case mod := <-coord.ch:
+			log.Info("warming athens with %s", mod)
+		}
+	}
+}
